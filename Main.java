49d216ea14381f6b67824788f6ebff6e66942646
sathi19 <mayapkumanan@gmail.com>
2025-04-01 20:59:28 +0000

    main changes p3

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 public class Main{
         static int dayNum = 0; 
+        static ArrayList<String> userInventory; 
     public static void main (String[] args){
         Scanner collector = new Scanner(System.in);
         System.out.println("Intro");
-        while(dayNum <= 12){
+        User user = new User("human", 50, 100, true, "name", userInventory);
+        while(dayNum <= 12 && user.getIsAlive()){
             if(dayNum == 12){
-                System.out.println("Your saved")
+                System.out.println("Your saved");
             }
             String userResponse = collector.nextString();
         }
