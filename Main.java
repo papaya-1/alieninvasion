4fc242c5a5ef707bd93bca5147a1a4b8c9e851d3
MarinnaG <marinnajg@gmail.com>
2025-04-16 21:33:54 +0000

    fixes

--- Main.java
+++ Main.java
@@ -4,12 +4,12 @@ public class Main{
         static int dayNum = 1; 
         static ArrayList<String> userInventory; 
         static boolean baseSelected = false;
-        static Base a = new Base("shack", 4, 4, 50, false); // 5x5 grid
-        static Base b = new Base("apartment", 1, 2, 50, false);
-        static Base c = new Base("house", 2, 0, 50, false);
+        static Base a = new Base(" shack", 4, 4, 50, false); // 5x5 grid
+        static Base b = new Base(" apartment", 1, 2, 50, false);
+        static Base c = new Base(" house", 2, 0, 50, false);
     public static void main (String[] args){
         Scanner collector = new Scanner(System.in);
-        System.out.println("You come from summer break to find that your town has been invaded by alien! No one else is around, but luckily help is arriving in 12 days. You'll need to find a base, maintain it, and get food and survive alien attacks to keep your health up until help arrives.");
+        System.out.println("You come from summer break to find that your town has been invaded by alien! No one else is around, but luckily help is arriving in 12 days. You'll need to find a base, maintain it, get food, and survive alien attacks to keep your health up until help arrives.");
         System.out.println("");
         User user = new User("human", 50, 100, true, userInventory);
         while(dayNum <= 12 && user.getIsAlive()){
