0258a87385ccffc7d030830778105161cf4f9985
MarinnaG <marinnajg@gmail.com>
2025-04-25 03:38:34 +0000

    encounter alien methode and  inventory stats option activity

--- User.java
+++ User.java
@@ -6,36 +6,28 @@ public class User extends Entities{
     public User(){
         super();
     }
-    
     public User(String type, int defense, int health, boolean isAlive, ArrayList <Item> inventory){
         super(type, defense, health, isAlive);
         this.inventory = inventory;
     }
-
     public void addDefense(int x){
         defense += x;
     }
-
     public void minusDefense(int x){
         defense -= x;
     }
-
     public void plusHealth(int x){
         health += x;
     }
-
     public void minusHealth(int x){
         health -= x;
     }
-
     public void changeLifeStatus(boolean x){
         isAlive = x;
     }
-
     public void addtoInventory(Item x){
         inventory.add(x);
     }
-
     public void removeItem(int num){
         int index = num - 1;//-1 because the list is numbered starting from 1 not 0, so the user will give the computer the index + 1 
         inventory.remove(index);
